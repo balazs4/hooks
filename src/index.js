9b74518603aac8d18c8a3606fa897590873fd0f1
balazs4 <balazs4web@gmail.com>
2018-10-28 12:05:02 +0100

    feat: custom hooks :heart:

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const useFetch = url => {
+  const [data, setData] = useState(null);
+  useEffect(
+    () => {
+      if (data === null)
+        sleep(Math.random() * 1000 * 2)
+          .then(() => window.fetch(url))
+          .then(x => x.json())
+          .then(x => setData(x[Object.keys(x)[0]]));
+    },
+    [data]
+  );
+  return data;
+};
+
+const useKrautipsum = (...resources) =>
+  resources.reduce(
+    (acc, res) => ({
+      ...acc,
+      [res]: useFetch(`http://krautipsum.de/api/${res}`)
+    }),
+    {}
+  );
+
+const App = props => {
+  const kraut = useKrautipsum('greeting', 'noun', 'verb', 'adjective');
+  return (
+    <div>
+      <pre>{JSON.stringify(kraut, null, 2)}</pre>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('app'));
