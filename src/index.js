cf637b6872c51e84968f39a05eb371bc5cdc77c6
balazs4 <balazs4web@gmail.com>
2018-10-28 13:55:05 +0100

    fix: extract `useKrautipsum` custom hook

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const useFetch = url => {
   const [data, setData] = useState(null);
   useEffect(
     () => {
       if (data === null)
-        sleep(Math.random() * 1000 * 2)
-          .then(() => window.fetch(url))
+        new Promise(resolve => setTimeout(resolve, Math.random() * 1000 * 2))
+          .then(() => fetch(url))
           .then(x => x.json())
           .then(x => setData(x[Object.keys(x)[0]]));
     },
@@ -17,20 +16,19 @@ const useFetch = url => {
   return data;
 };
 
-const useKrautipsum = (...resources) =>
-  resources.reduce(
-    (acc, res) => ({
-      ...acc,
-      [res]: useFetch(`https://krautipsum.de/api/${res}`)
-    }),
-    {}
-  );
+const useKrautipsum = () => {
+  const greeting = useFetch(`https://krautipsum.de/api/greeting`);
+  const verb = useFetch(`https://krautipsum.de/api/verb`);
+  const noun = useFetch(`https://krautipsum.de/api/noun`);
+  const adjective = useFetch(`https://krautipsum.de/api/adjective`);
+  return { greeting, adjective, noun, verb };
+};
 
 const App = props => {
-  const kraut = useKrautipsum('greeting', 'noun', 'verb', 'adjective');
+  const krautipsum = useKrautipsum();
   return (
     <div>
-      <pre>{JSON.stringify(kraut, null, 2)}</pre>
+      <pre>{JSON.stringify(krautipsum, null, 2)}</pre>
     </div>
   );
 };
